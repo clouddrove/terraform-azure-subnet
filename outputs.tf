c47b15daa69bc7b7a12e630b48a40f8e85f17c11
Rupal Sharma <152851038+Rupalgw@users.noreply.github.com>
2024-02-19 18:25:42 +0530

    ifx: Count condition in outputs.tf (#33)

--- outputs.tf
+++ outputs.tf
@@ -1,50 +1,50 @@
 output "specific_subnet_name" {
-  value       = azurerm_subnet.subnet[*].name
+  value       = try(azurerm_subnet.subnet[*].name, null)
   description = "The name of the subnet."
 }
 
 output "specific_subnet_id" {
-  value       = azurerm_subnet.subnet[*].id
+  value       = try(azurerm_subnet.subnet[*].id, null)
   description = "The name of the subnet."
 }
 
 output "specific_subnet_address_prefixes" {
+  value       = try(azurerm_subnet.subnet[*].address_prefixes, null)
   description = "The address prefixes for the subnet."
-  value       = azurerm_subnet.subnet[*].address_prefixes
 }
 
 output "default_subnet_name" {
-  value       = azurerm_subnet.subnet[*].name
+  value       = try(azurerm_subnet.subnet[*].name, null)
   description = "The name of the subnet."
 }
 
 output "default_subnet_id" {
+  value       = try(azurerm_subnet.subnet[*].id, null)
   description = "The subnet ID."
-  value       = azurerm_subnet.subnet[*].id
 }
 
 output "default_subnet_address_prefixes" {
-  value       = azurerm_subnet.subnet[*].address_prefixes
+  value       = try(azurerm_subnet.subnet[*].address_prefixes, null)
   description = "The address prefixes for the subnet."
 }
 
 output "nat_gateway_id" {
-  value       = azurerm_nat_gateway.natgw[*].id
+  value       = try(azurerm_nat_gateway.natgw[0].id, null)
   description = "The ID of the NAT Gateway."
 }
 
 output "public_ip_address" {
-  value       = azurerm_public_ip.pip[*].ip_address
+  value       = try(azurerm_public_ip.pip[0].ip_address, null)
   description = "The IP address value that was allocated."
 }
 
 output "public_ip_id" {
-  value       = azurerm_public_ip.pip[*].id
+  value       = try(azurerm_public_ip.pip[0].id, null)
   description = " The ID of this Public IP."
 }
 
 output "route_table_id" {
-  value       = var.enable && var.enable_route_table ? azurerm_route_table.rt[*].id : null
+  value       = var.enable && var.enable_route_table ? azurerm_route_table.rt[0].id : null
   description = "The Route Table ID."
 }
 
